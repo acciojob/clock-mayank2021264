ae2f56bc34a4c4c1df11b2aa24cc6d21f1624b5c
MAYANK PANDEY <101644584+mayank2021264@users.noreply.github.com>
2026-04-14 12:52:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function updateTime(){
+	let now = new Date();
+	let display = document.getElementById('timer');
+	display.textContent = `${now.toLocaleDateString()},
+							${now.toLocaleTimeString()}`;
+}
+
+setInterval(updateTime, 1000);
+
+updateTime();
